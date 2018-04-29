dddc10bad03a7307c176ed2d65a7936b58041006
Allan Glasier <allan@cybernetixdesign.com>
2018-04-29 02:25:17 -0400

    Built CLI

--- cineplex_showtimes.gemspec
+++ cineplex_showtimes.gemspec
@@ -11,7 +11,7 @@ Gem::Specification.new do |spec|
 
   spec.summary       = "Display showtimes for local Cineplex theatre"
   spec.description   = "Scrape cineplex.com for local showtimes and movie info"
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.homepage      = "https://github.com/allan-glasier/cineplex_showtimes"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
--- lib/cineplex_showtimes/cli.rb
+++ lib/cineplex_showtimes/cli.rb
@@ -1,15 +1,35 @@
 class CineplexShowtimes::CLI
 
   def call
-    puts "test"
+    showtime_list
+    menu
+    goodbye
   end
 
   def showtime_list
-
+    @showtimes = CineplexShowtimes::Showtimes.showtimes
+    @showtimes.each.with_index(1) {|movie, index| puts "#{i}. #{movie.name}"}
   end
 
   def menu
+    input = ""
+    while input != "exit"
+      puts "Enter the number of the movie you would like to know more about, type 'list' to display the list of showtimes again or type 'exit'"
+      input = gets.strip.downcase
 
+      if input.to_i > 0
+        showtime = @showtimes[input.to_i - 1]
+        puts "#{showtime.name}"
+      elsif input == "list"
+        showtime_list
+      else
+        puts "Unrecognized command, please enter 'list' or 'exit'"
+      end
+    end
+  end
+
+  def goodbye
+    puts "Thank you for using the Cineplex Showtimes gem, have yourself a great day!"
   end
 
 end
