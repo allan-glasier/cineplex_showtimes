a5c20a404fb85aff5736f0b6d233289455959baf
Allan Glasier <allan@cybernetixdesign.com>
2018-04-29 15:19:56 -0400

    Scraper is now working
    Now figuring out how to sort the time array

--- lib/cineplex_showtimes/cli.rb
+++ lib/cineplex_showtimes/cli.rb
@@ -1,35 +1,51 @@
 class CineplexShowtimes::CLI
 
   def call
+    get_showtime_list
     showtime_list
-    menu
-    goodbye
+    menu    
   end
 
-  def showtime_list
-    @showtimes = CineplexShowtimes::Showtimes.showtimes
-    @showtimes.each.with_index(1) {|showtime, index| puts "#{index}. #{showtime.movie}"}
+  def get_showtime_list
+    @showtimes = CineplexShowtimes::Showtimes.showtimes    
+  end
+
+  def showtime_list    
+    puts " "
+    @showtimes.each.with_index(1) {|showtime, index| puts "#{index}. #{showtime.movie[0]}"}
   end
 
   def menu
     input = ""
     while input != "exit"
-      puts "Enter the number of the movie you would like to know more about, type 'list' to display the list of showtimes again or type 'exit'"
+      puts "\nEnter the number of the movie you would like to know more about, type 'list' to display the list of showtimes again or type 'exit'\n "
       input = gets.strip.downcase
-
-      if input.to_i > 0
+      
+      if input.to_i > 0 
         showtime = @showtimes[input.to_i - 1]
-        puts "#{showtime.name}"
+        puts " "
+        puts <<~DOC
+          #{showtime.movie[0]} | #{showtime.runtime} 
+           
+          Rated: "#{showtime.rating}" for #{showtime.rating_description}
+
+          Showtimes: #{showtime.time.sort.join(", ")}
+
+          Synopsis:
+          #{showtime.synopsis}
+        DOC
       elsif input == "list"
         showtime_list
+      elsif input == "exit"
+        goodbye      
       else
-        puts "Unrecognized command, please enter 'list' or 'exit'"
-      end
+        puts "\nUnrecognized command, please enter 'list' or 'exit'\n "
+      end      
     end
   end
 
   def goodbye
-    puts "Thank you for using the Cineplex Showtimes gem, have yourself a great day!"
+    puts "\n Thank you for using the Cineplex Showtimes gem, have yourself a great day!\n "
   end
 
 end
--- lib/cineplex_showtimes/showtimes.rb
+++ lib/cineplex_showtimes/showtimes.rb
@@ -1,41 +1,29 @@
 class CineplexShowtimes::Showtimes
-  attr_accessor :movie, :runtime, :time, :rating, :rating_description, :synopsis
+  attr_accessor :movie, :runtime, :time, :rating, :rating_description, :synopsis  
+
+  @@showtimes = []
 
   def self.showtimes
     self.scrape_showtimes
+    binding.pry
   end
 
-  def self.scrape_showtimes
-    showtimes = []
-
-    showtimes << self.scrape_silvercity
-    showtimes << self.scrape_odeon
-
-    showtimes
-  end
-
-  def self.scrape_silvercity
+    def self.scrape_showtimes
     date = DateTime.now.strftime("%m/%d/%Y")
     domain = "https://www.cineplex.com"
-    html = Nokogiri::HTML(open("https://www.cineplex.com/Showtimes/any-movie/cineplex-odeon-devonshire-mall-cinemas?Date=#{date}"))
-    showtimes = {}  
+    html = Nokogiri::HTML(open("https://www.cineplex.com/Showtimes/any-movie/cineplex-odeon-devonshire-mall-cinemas?Date=#{date}"))    
 
     html.css(".showtime-card").each do |movie_object|      
       synopsis_link = movie_object.css(".movie-details-link-click").attribute("href").value            
-      showtimes = {
-        :movie => movie_object.css(".h3 a").text.strip,
-        :runtime => movie_object.css(".h3 span").text.gsub("| ", ""),
-        :time => movie_object.css(".showtime--list a").text.gsub(" ", "").gsub("\n", "").split("\r").reject(&:empty?).sort,
-        :rating => movie_object.css(".movie-header-details p meta").attribute("content").value,
-        :rating_description => movie_object.css(".movie-header-details p:first-child").text.strip,
-        :synopsis => Nokogiri::HTML(open("#{domain}#{synopsis_link}")).css(".md-movie-info div:first-child p").text
-      }
+        showtime = self.new         
+        showtime.movie = movie_object.css(".h3 a").text.strip,
+        showtime.runtime = movie_object.css(".h3 span").text.gsub("| ", ""),
+        showtime.time = movie_object.css(".showtime--list a").text.gsub(" ", "").gsub("\n", "").split("\r").reject(&:empty?),
+        showtime.rating = movie_object.css(".movie-header-details p meta").attribute("content").value,
+        showtime.rating_description = movie_object.css(".movie-header-details p:first-child").text.strip,
+        showtime.synopsis = Nokogiri::HTML(open("#{domain}#{synopsis_link}")).css(".md-movie-info div:first-child p").text
+        @@showtimes << showtime      
     end
-    showtimes
+    @@showtimes       
   end
-
-  def self.scrape_odeon
-
-  end
-
 end
